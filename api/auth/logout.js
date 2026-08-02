ca1d586829e6652bf023ea100493954c51d0a793
tanoshinderu336699 <thanks.for.all777@gmail.com>
2026-08-02 19:49:42 +0900

    Implement logout endpoint to clear session cookie

---
+++ api/auth/logout.js
@@ -0,0 +1,6 @@
+const { clearSessionCookie } = require('../../lib/session');
+
+module.exports = async (req, res) => {
+  res.setHeader('Set-Cookie', clearSessionCookie());
+  res.status(200).json({ ok: true });
+};
